368aad926b9b4936da112e11ec769b99f16dbf20
zhguoiub <zhguoiub@f4b6d9d8-0936-11de-8884-7b8501395459>
2009-03-13 23:41:55 +0000

    build a maven project for shindig part

---
+++ docsim-shingle/src/main/java/edu/indiana/cs/docsim/App.java
@@ -0,0 +1,13 @@
+package edu.indiana.cs.docsim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
