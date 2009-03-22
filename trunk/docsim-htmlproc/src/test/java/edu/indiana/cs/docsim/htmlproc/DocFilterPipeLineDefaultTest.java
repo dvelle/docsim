60e89f1ba1d0aaabfaf56ed8a6f972f40dd598cf
zhguoiub <zhguoiub@f4b6d9d8-0936-11de-8884-7b8501395459>
2009-03-22 04:57:27 +0000

    Add missing files

---
+++ trunk/docsim-htmlproc/src/test/java/edu/indiana/cs/docsim/htmlproc/DocFilterPipeLineDefaultTest.java
@@ -0,0 +1,62 @@
+package edu.indiana.cs.docsim.htmlproc;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.IOUtils;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class DocFilterPipeLineDefaultTest
+  extends TestCase {
+    private static Logger logger =
+        Logger.getLogger(DocFilterPipeLineDefaultTest.class.getName());
+
+    private DocFilterPipeLineDefault docFilterPipeLine;
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DocFilterPipeLineDefaultTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(DocFilterPipeLineDefaultTest.class);
+    }
+
+    @Override
+    public void setUp() {
+        try {
+            docFilterPipeLine = new DocFilterPipeLineDefault();
+        } catch(Exception ex) {
+            logger.severe("Exception in creating DocFilterPipeLineDefault " +
+                    ex);
+        }
+    }
+
+    public void testPipeLineLocal() throws Exception{
+        String filename = "res://augmented_page.html";
+        String charset = "UTF-8";
+        String result = docFilterPipeLine.filter(filename, charset);
+        logger.info("Get data from file \"" + filename + "\" result:\n" + result);
+    }
+
+    public void testPipeLineRemote() throws Exception{
+        String strUrl = "http://cs.indiana.edu";
+        String charset = "UTF-8";
+        URL url = new URL(strUrl);
+        String result = docFilterPipeLine.filter(url);
+        logger.info("Get page from " + strUrl + ".\n Filtering result:\n" + result);
+    }
+}
+
+
